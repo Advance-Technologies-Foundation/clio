d9c330c244eb60fdd2a42e2d82b67a70a28ab041
Pavel Shvedun <p.shvedun@bpmonline.com>
2018-09-23 16:47:46 +0300

    #IDEBPMONLINE-38 Реализовать метод compress в bpmcli которая на основе папки с проектом пакета сформирует пакет bpmonline в формате gz без лишних файлов

--- bpmcli/Program.cs
+++ bpmcli/Program.cs
@@ -183,9 +183,11 @@ namespace bpmcli
 			if (File.Exists(destinationPath)) {
 				File.Delete(destinationPath);
 			}
+			string tempPath = CreateTempPath(sourcePath);
+			CopyProjectFiles(sourcePath, tempPath);
 
-			string[] files = Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories);
-			int directoryPathLength = sourcePath.Length;
+			string[] files = Directory.GetFiles(tempPath, "*.*", SearchOption.AllDirectories);
+			int directoryPathLength = tempPath.Length;
 			using (Stream fileStream =
 				File.Open(destinationPath, FileMode.Create, FileAccess.Write, FileShare.None)) {
 				using (var zipStream = new GZipStream(fileStream, CompressionMode.Compress)) {
@@ -194,7 +196,48 @@ namespace bpmcli
 					}
 				}
 			}
+			Directory.Delete(tempPath, true);
+		}
 
+		private static string CreateTempPath(string sourcePath) {
+			var directoryInfo = new DirectoryInfo(sourcePath);
+			string tempPath = Path.Combine(Path.GetTempPath(), directoryInfo.Name);
+			return tempPath;
+		}
+
+		private static void CopyProjectFiles(string sourcePath, string destinationPath) {
+			if (Directory.Exists(destinationPath)) {
+				Directory.Delete(destinationPath, true);
+			}
+			Directory.CreateDirectory(destinationPath);
+			CopyProjectElement(sourcePath, destinationPath, "Assemblies");
+			CopyProjectElement(sourcePath, destinationPath, "Bin");
+			CopyProjectElement(sourcePath, destinationPath, "Data");
+			CopyProjectElement(sourcePath, destinationPath, "Files");
+			CopyProjectElement(sourcePath, destinationPath, "Resources");
+			CopyProjectElement(sourcePath, destinationPath, "Schemas");
+			CopyProjectElement(sourcePath, destinationPath, "SqlScripts");
+			File.Copy(Path.Combine(sourcePath, "descriptor.json"), Path.Combine(destinationPath, "descriptor.json"));
+		}
+
+		internal static void CopyProjectElement(string sourcePath, string destinationPath, string name) {
+			string fromAssembliesPath = Path.Combine(sourcePath, name);
+			if (Directory.Exists(fromAssembliesPath)) {
+				string toAssembliesPath = Path.Combine(destinationPath, name);
+				CopyDir(fromAssembliesPath, toAssembliesPath);
+			}
+		}
+
+		internal static void CopyDir(string source, string dest) {
+			if (String.IsNullOrEmpty(source) || String.IsNullOrEmpty(dest))
+				return;
+			Directory.CreateDirectory(dest);
+			foreach (string fn in Directory.GetFiles(source)) {
+				File.Copy(fn, Path.Combine(dest, Path.GetFileName(fn)), true);
+			}
+			foreach (string dirFn in Directory.GetDirectories(source)) {
+				CopyDir(dirFn, Path.Combine(dest, Path.GetFileName(dirFn)));
+			}
 		}
 
 		private static void InstallPackage(string filePath) {
