cfbffc317d5d8e726d677d1d503c8095df7d485d
alexbondj <alexbondj@gmail.com>
2018-11-09 18:11:09 +0200

    #рефакторинг класса

--- bpmcli/BpmPkgProject.cs
+++ bpmcli/BpmPkgProject.cs
@@ -10,8 +10,9 @@ namespace bpmcli
 {
 	public class BpmPkgProject
 	{
-		private static XNamespace _xNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
-		private const string IncludeAttributeName = "Include";
+		private const string HintElementName = "HintPath";
+		private const string ItemGroupElemetName = "ItemGroup";
+		private const string ReferenceElementName = "Reference";
 
 		private const string PathToBinDebug = @"..\..\..\..\..\..\..\Bin\Debug\";
 
@@ -19,42 +20,53 @@ namespace bpmcli
 		private BpmPkgProject(string path) {
 			LoadPath = path;
 			Document = XElement.Load(path, LoadOptions.PreserveWhitespace);
+			Namespace = Document.Name.Namespace;
 		}
+
+		private XName HintPath => Namespace + HintElementName;
+
+		private XName ItemGroup => Namespace + ItemGroupElemetName;
+
+		private XName Reference => Namespace + ReferenceElementName;
+
+		private static XName IncludeAttr => "Include";
 
 		public XElement Document { get; }
 
 		public string LoadPath { get; }
+
+		public XNamespace Namespace { get; }
 
 		public static BpmPkgProject LoadFromFile(string path) {
 			return new BpmPkgProject(path);
 		}
 
 		private void ChangeHint(XElement element) {
-			var value = element.Attribute(IncludeAttributeName)?.Value;
+			var value = element.Attribute(IncludeAttr)?.Value;
 			if (value != null) {
 				int index = value.IndexOf(",", StringComparison.Ordinal);
 				if (index > 0) {
 					value = value.Substring(0, index);
-					element.SetAttributeValue(IncludeAttributeName, value);
+					element.SetAttributeValue(IncludeAttr, value);
 				}
 			}
-			var hintValue = element.Element(_xNamespace + "HintPath")?.Value;
+			var hintValue = element.Element(HintPath)?.Value;
 			if (hintValue != null) {
 				int index = hintValue.LastIndexOf("\\", StringComparison.Ordinal);
 				if (index > 0) {
 					hintValue = string.Concat(PathToBinDebug, hintValue.Substring(index).TrimStart('\\'));
-					element.SetElementValue(_xNamespace + "HintPath", hintValue);
+					element.SetElementValue(HintPath, hintValue);
 				}
 			}
 		}
 
 		public BpmPkgProject RebaseToCoreDebug() {
 			List<XElement> rebaseElements = new List<XElement>();
-			foreach (var el in Document.Elements(_xNamespace + "ItemGroup")) {
-				rebaseElements.AddRange(el.Elements(_xNamespace + "Reference")
+			foreach (var el in Document.Elements(ItemGroup)) {
+				rebaseElements.AddRange(el.Elements(Reference)
 					.Where(elem => (
-						elem.Element(_xNamespace + "HintPath") != null &&
-						((string)elem.Element(_xNamespace + "HintPath")).Contains("BpmonlineSDK"))));
+						elem.Element(HintPath) != null &&
+						((string)elem.Element(HintPath)).Contains("BpmonlineSDK"))));
 			}
 			rebaseElements.ForEach(ChangeHint);
 			return this;
