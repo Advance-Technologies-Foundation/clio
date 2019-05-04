c36b05d74e806185ffbfc615abdd50a96c3ca961
Vladimir <nikonov.volodymyr@gmail.com>
2019-05-04 17:14:21 +0300

    update bpmcligate sql endpoint

--- bpmcligate/Files/cs/BpmcliApiGateway.cs
+++ bpmcligate/Files/cs/BpmcliApiGateway.cs
@@ -11,31 +11,29 @@ using Terrasoft.Web.Common;
 
 namespace bpmcligate.Files.cs
 {
-    [ServiceContract]
-    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
-    class BpmcliApiGateway : BaseService
-    {
-        [OperationContract]
-        [WebInvoke(Method = "POST", UriTemplate = "ExecuteSqlScript", BodyStyle = WebMessageBodyStyle.Wrapped,
-            RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-        public string ExecuteSqlScript(string script)
-        {
-            if (UserConnection.DBSecurityEngine.GetCanExecuteOperation("CanManageSolution"))
-            {
-                return SQLFunctions.ExecuteSQL(script, UserConnection);
-            }
-            else { throw new Exception("You don`n have permission for operation CanManageSolution"); }
-        }
+	[ServiceContract]
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+	class BpmcliApiGateway : BaseService
+	{
+		[OperationContract]
+		[WebInvoke(Method = "POST", UriTemplate = "ExecuteSqlScript", BodyStyle = WebMessageBodyStyle.WrappedRequest,
+			RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+		public string ExecuteSqlScript(string script) {
+			if (UserConnection.DBSecurityEngine.GetCanExecuteOperation("CanManageSolution")) {
+				return SQLFunctions.ExecuteSQL(script, UserConnection);
+			} else {
+				throw new Exception("You don`n have permission for operation CanManageSolution");
+			}
+		}
 
-        [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "GetApiVersion", RequestFormat = WebMessageFormat.Json,
-            ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest)]
-        public string GetApiVersion()
-        {
-            var version = typeof(BpmcliApiGateway).Assembly.GetName().Version;
-            return version.ToString();
-        }
-    }
+		[OperationContract]
+		[WebInvoke(Method = "GET", UriTemplate = "GetApiVersion", RequestFormat = WebMessageFormat.Json,
+			ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+		public string GetApiVersion() {
+			var version = typeof(BpmcliApiGateway).Assembly.GetName().Version;
+			return version.ToString();
+		}
+	}
 }
 
 
