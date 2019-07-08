fb52e39fc6de7c75f26b6ee5e8affd22377ab57e
alexbondj <alexbondj@gmail.com>
2019-07-08 21:23:45 +0300

    #71 Реализована команда по установке фичи по коду

---
+++ bpmcli/Feature/FeatureModerator.cs
@@ -0,0 +1,44 @@
+﻿namespace bpmcli.Feature
+{
+	using System;
+	using Bpmonline.Client;
+
+	public class FeatureModerator
+	{
+		private const string FeatureServiceName = "FeatureService";
+		private const string CreateMethodName = "CreateFeature";
+		private const string SetFeatureStateMethodName = "SetFeatureState";
+		
+
+		protected BpmonlineClient BpmonlineClient;
+
+		public FeatureModerator(BpmonlineClient client) {
+			BpmonlineClient = client;
+		}
+
+		public void SwitchFeatureOn(string code) {
+			Feature feature = Feature.CreateFeatureWithCode(code).TurnOn();
+			CallCreateFeature(feature).CallChangeFeatureState(feature);
+		}
+
+		public void SwitchFeatureOff(string code) {
+			Feature feature = Feature.CreateFeatureWithCode(code).TurnOff();
+			CallCreateFeature(feature).CallChangeFeatureState(feature);
+		}
+
+		private FeatureModerator CallCreateFeature(Feature feature) {
+			string requestData = "{" +
+				$"\"code\":\"{feature.Code}\",\"name\":\"{feature.Name}\",\"description\":\"{feature.Descriptor}\"" + "}";
+			BpmonlineClient.CallConfigurationService(FeatureServiceName, CreateMethodName, requestData);
+			return this;
+		}
+
+		private FeatureModerator CallChangeFeatureState(Feature feature) {
+			string requestData = "{" +
+				$"\"code\":\"{feature.Code}\",\"state\":\"{(int)feature.State}\"" + "}";
+			BpmonlineClient.CallConfigurationService(FeatureServiceName, SetFeatureStateMethodName, requestData);
+			return this;
+		}
+
+	}
+}
