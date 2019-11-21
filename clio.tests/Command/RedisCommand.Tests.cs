0914b8a2c155b26f3d492bbfdfa166e15e42c9ed
Aleksandr Davydko <a.davydko@bpmonline.com>
2019-11-21 20:10:14 +0200

    #0 Дозалил юнит тесты

---
+++ clio.tests/Command/RedisCommand.Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace Clio.Tests.Command
+{
+	using Clio.Command.RedisCommand;
+	using Clio.Common;
+	using NSubstitute;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class RedisCommandTestCase
+	{
+		[Test, Category("Unit")]
+		public void ClearRedisDb_FormsCorrectApplicationRequest_WhenApplicationRunsUnderNetFramework() {
+			IApplicationClient applicationClient = Substitute.For<IApplicationClient>();
+			RedisCommand redisCommand = new RedisCommand(applicationClient);
+			var restartOptions = new ClearRedisOptions {
+				Login = "Test",
+				Password = "Test",
+				IsNetCore = false,
+				Maintainer = "Test",
+				Uri = "http://test.domain.com"
+			};
+			redisCommand.ClearRedisDb(restartOptions);
+			applicationClient.Received(1).ExecutePostRequest(
+				restartOptions.Uri + "/0/ServiceModel/AppInstallerService.svc/ClearRedisDb",
+				"{}");
+		}
+
+		[Test, Category("Unit")]
+		public void ClearRedisDb_FormsCorrectApplicationRequest_WhenApplicationRunsUnderNetCore() {
+			IApplicationClient applicationClient = Substitute.For<IApplicationClient>();
+			RedisCommand redisCommand = new RedisCommand(applicationClient);
+			var restartOptions = new ClearRedisOptions {
+				Login = "Test",
+				Password = "Test",
+				IsNetCore = true,
+				Maintainer = "Test",
+				Uri = "http://test.domain.com"
+			};
+			redisCommand.ClearRedisDb(restartOptions);
+			applicationClient.Received(1).ExecutePostRequest(
+				restartOptions.Uri + "/ServiceModel/AppInstallerService.svc/ClearRedisDb",
+				"{}");
+		}
+	}
+}
---
+++ clio.tests/Command/RestartCommand.Tests.cs
@@ -0,0 +1,79 @@
+﻿namespace Clio.Tests.Command
+{
+	using Clio.Command.RestartCommand;
+	using Clio.Common;
+	using NSubstitute;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class RestartCommandTestCase
+	{
+		[Test, Category("Unit")]
+		public void RestartCommand_FormsCorrectApplicationRequest_WhenApplicationRunsUnderNetFramework() {
+			IApplicationClient applicationClient = Substitute.For<IApplicationClient>();
+			RestartCommand restartCommand = new RestartCommand(applicationClient);
+			var restartOptions = new RestartOptions {
+				Login = "Test",
+				Password = "Test",
+				IsNetCore = false,
+				Maintainer = "Test",
+				Uri = "http://test.domain.com"
+			};
+			restartCommand.Restart(restartOptions);
+			applicationClient.Received(1).ExecutePostRequest(
+				restartOptions.Uri + "/0/ServiceModel/AppInstallerService.svc/UnloadAppDomain",
+				"{}");
+		}
+
+		[Test, Category("Unit")]
+		public void RestartCommand_FormsCorrectApplicationRequest_WhenApplicationRunsUnderNetFrameworkAndSettingsPickedFromEnvironment() {
+			IApplicationClient applicationClient = Substitute.For<IApplicationClient>();
+			RestartCommand restartCommand = new RestartCommand(applicationClient);
+			var environmentSettings = new EnvironmentSettings {
+				Login = "Test",
+				Password = "Test",
+				IsNetCore = false,
+				Maintainer = "Test",
+				Uri = "http://test.domain.com"
+			};
+			restartCommand.Restart(environmentSettings);
+			applicationClient.Received(1).ExecutePostRequest(
+				environmentSettings.Uri + "/0/ServiceModel/AppInstallerService.svc/UnloadAppDomain",
+				"{}");
+		}
+
+		[Test, Category("Unit")]
+		public void RestartCommand_FormsCorrectApplicationRequest_WhenApplicationRunsUnderNetCore() {
+			IApplicationClient applicationClient = Substitute.For<IApplicationClient>();
+			RestartCommand restartCommand = new RestartCommand(applicationClient);
+			var restartOptions = new RestartOptions {
+				Login = "Test",
+				Password = "Test",
+				IsNetCore = true,
+				Maintainer = "Test",
+				Uri = "http://test.domain.com"
+			};
+			restartCommand.Restart(restartOptions);
+			applicationClient.Received(1).ExecutePostRequest(
+				restartOptions.Uri + "/ServiceModel/AppInstallerService.svc/RestartApp",
+				"{}");
+		}
+
+		[Test, Category("Unit")]
+		public void RestartCommand_FormsCorrectApplicationRequest_WhenApplicationRunsUnderNetCoreAndSettingsPickedFromEnvironment() {
+			IApplicationClient applicationClient = Substitute.For<IApplicationClient>();
+			RestartCommand restartCommand = new RestartCommand(applicationClient);
+			var environmentSettings = new EnvironmentSettings {
+				Login = "Test",
+				Password = "Test",
+				IsNetCore = true,
+				Maintainer = "Test",
+				Uri = "http://test.domain.com"
+			};
+			restartCommand.Restart(environmentSettings);
+			applicationClient.Received(1).ExecutePostRequest(
+				environmentSettings.Uri + "/ServiceModel/AppInstallerService.svc/RestartApp",
+				"{}");
+		}
+	}
+}
