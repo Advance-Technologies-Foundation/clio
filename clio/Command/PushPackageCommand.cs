c9aef8907b14c81e8c0d813fcfeff71a1722f817
Vladimir <nikonov.volodymyr@gmail.com>
2020-03-09 16:18:49 +0200

    Update PushPackageCommand.cs

--- clio/Command/PushPackageCommand.cs
+++ clio/Command/PushPackageCommand.cs
@@ -3,9 +3,9 @@ using System.IO;
 using System.Text;
 using System.Threading;
 using Clio.Common;
+using Clio.Package;
 using Clio.UserEnvironment;
 using CommandLine;
-using Newtonsoft.Json.Linq;
 
 namespace Clio.Command
 {
@@ -19,107 +19,23 @@ namespace Clio.Command
 		public string ReportPath { get; set; }
 	}
 
-	public class PushPackageCommand : RemoteCommand<PushPkgOptions>
+	public class PushPackageCommand : Command<PushPkgOptions>
 	{
-		private readonly IProjectUtilities _projectUtilities;
-		private readonly ISettingsRepository _settingsRepository;
-		private readonly ISqlScriptExecutor _scriptExecutor;
+		public PushPackageCommand(IPackageInstaller packageInstaller) {
+			_packageInstaller = packageInstaller;
 
-		private static string InstallUrl => @"/ServiceModel/PackageInstallerService.svc/InstallPackage";
-		private static string LogUrl => @"/ServiceModel/PackageInstallerService.svc/GetLogFile";
-		private static string UploadUrl => @"/ServiceModel/PackageInstallerService.svc/UploadPackage";
-		private static string DefLogFileName => "cliolog.txt";
-
-		public PushPackageCommand(IApplicationClient applicationClient, EnvironmentSettings settings,
-				IProjectUtilities projectUtilities, ISettingsRepository settingsRepository,
-				ISqlScriptExecutor scriptExecutor)
-			: base(applicationClient, settings) {
-			_projectUtilities = projectUtilities;
-			_settingsRepository = settingsRepository;
-			_scriptExecutor = scriptExecutor;
 		}
-
-		private string GetCompleteUrl(string url) {
-			return EnvironmentSettings.Uri + (EnvironmentSettings.IsNetCore ? string.Empty : "/0/") + url;
-		}
-
-		private void UnlockMaintainerPackageInternal(EnvironmentSettings settings) {
-			var script = $"UPDATE SysPackage SET InstallType = 0 WHERE Maintainer = '{settings.Maintainer}'";
-			_scriptExecutor.Execute(script, ApplicationClient, settings);
-		}
-
-		private (bool, string) InstallPackage(string filePath, EnvironmentOptions options) {
-			string fileName;
-			try {
-				fileName = UploadPackage(filePath);
-			} catch (Exception e) {
-				Console.WriteLine(e.Message);
-				return (false, e.Message);
-			}
-			Console.WriteLine($"Install {fileName} ...");
-			string result = ApplicationClient.ExecutePostRequest(GetCompleteUrl(InstallUrl), "\"" + fileName + "\"", Timeout.Infinite);
-			dynamic resultObj = JObject.Parse(result);
-			var settings = _settingsRepository.GetEnvironment(options);
-			if (settings.DeveloperModeEnabled.HasValue && settings.DeveloperModeEnabled.Value) {
-				UnlockMaintainerPackageInternal(settings);
-				new RestartCommand(ApplicationClient, EnvironmentSettings).Execute(new RestartOptions());
-			}
-			var logText = GetLog();
-			Console.WriteLine("Installation log:");
-			Console.WriteLine(logText);
-			return (resultObj.success, logText);
-		}
-
-		private string GetLog() {
-			return ApplicationClient.ExecuteGetRequest(GetCompleteUrl(LogUrl));
-		}
-
-		private string UploadPackage(string filePath) {
-			Console.WriteLine("Uploading...");
-			FileInfo fileInfo = new FileInfo(filePath);
-			string fileName = fileInfo.Name;
-			ApplicationClient.UploadFile(GetCompleteUrl(UploadUrl), filePath);
-			Console.WriteLine("Uploaded");
-			return fileName;
-		}
-
-		private void SaveLogFile(string logText, string reportPath) {
-			if (File.Exists(reportPath)) {
-				File.Delete(reportPath);
-				File.WriteAllText(reportPath, logText, Encoding.UTF8);
-			} else if (Directory.Exists(reportPath)) {
-				reportPath = Path.Combine(reportPath, DefLogFileName);
-			}
-			File.WriteAllText(reportPath, logText, Encoding.UTF8);
-		}
+		private readonly IPackageInstaller _packageInstaller;
 
 		public override int Execute(PushPkgOptions options) {
-			var result = (success: false, logText: String.Empty);
 			try {
-				if (options.Name == null) {
-					options.Name = Environment.CurrentDirectory;
-				}
-				string logText = string.Empty;
-				if (File.Exists(options.Name)) {
-					result = InstallPackage(options.Name, options);
-				} else if (Directory.Exists(options.Name)) {
-					var folderPath = options.Name;
-					var filePath = options.Name + ".gz";
-					_projectUtilities.CompressProject(folderPath, filePath, false);
-					result = InstallPackage(filePath, options);
-					File.Delete(filePath);
-				} else {
-					Console.WriteLine("Specified package not found.");
-				}
+				_packageInstaller.Install(options.Name, options.ReportPath);
 				Console.WriteLine("Done");
+				return 0;
 			} catch (Exception e) {
 				Console.WriteLine(e.Message);
+				return 1;
 			}
-			if (options.ReportPath != null)
-			{
-				SaveLogFile(result.logText, options.ReportPath);
-			}
-			return (result.success) ? 0 : 1;
 		}
 	}
 }
