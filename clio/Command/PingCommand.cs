16c6a535f61f2b1f2f63ed0809fd4ab08c7c0f33
Kyryl Krylov, CPA <k.krylov@creatio.com>
2021-09-16 14:17:40 +0300

    Revert "Changed Ping command to support new health-check endpoints for .net Framework and .Net Core"
    This reverts commit f6370eb2051b1dd4c008e9f790c575ab86621527.

--- clio/Command/PingCommand.cs
+++ clio/Command/PingCommand.cs
@@ -4,17 +4,11 @@ using CommandLine;
 
 namespace Clio.Command
 {
-	enum AppType
-    {
-		WebApp, 
-		WebAppLoader
-    }
-
 	[Verb("ping-app", Aliases = new string[] { "ping" }, HelpText = "Check current credentional for selected environments")]
 	public class PingAppOptions : EnvironmentNameOptions
 	{
 		[Option('x', "Endpoint", Required = false, HelpText = "Relative path for checked endpoint (by default ise Ping service)")]
-		public string Endpoint { get; set; } = "/api/HealthCheck/Ping";
+		public string Endpoint { get; set; } = "/ping";
 	}
 
 	public class PingAppCommand : RemoteCommand<PingAppOptions>
@@ -23,60 +17,10 @@ namespace Clio.Command
 			: base(applicationClient, settings) {
 		}
 
-		/// <summary>
-		/// Pings WebAppLoader and WebApp
-		/// </summary>
-		/// <param name="options"></param>
-		/// <returns></returns>
-		/// <remarks>
-		/// See <see href="https://creatio.atlassian.net/wiki/spaces/TER/pages/2385216480/Endpoint+Creatio">new endpoint details</see>
-		/// </remarks>
 		public override int Execute(PingAppOptions options) {
-
-			PingApp(options, AppType.WebAppLoader);
-			PingApp(options, AppType.WebApp);
-			return 0;
-		}
-
-		private void PingApp(PingAppOptions options, AppType appType)
-        {
-			// Core and framework have the same endpoint
-			// WebAppLoader - 0/api/HealthCheck/Ping
-			// WebApp       - api/HealthCheck/Ping
-			// I use IsNetCore proprty to infulence ServiceUri 
-			// See RemoteCommand.ServiceUri for details
-			if(appType == AppType.WebAppLoader)
-            {
-				EnvironmentSettings.IsNetCore = true;
-				ServicePath = options.Endpoint;
-				Console.WriteLine($"Ping WebAppLoader {ServiceUri} ...");
-            }
-            else
-            {
-				EnvironmentSettings.IsNetCore = false;
-				ServicePath = options.Endpoint;
-				Console.WriteLine($"Ping WebApp       {ServiceUri} ...");
-			}
-
-			string result = ApplicationClient.ExecuteGetRequest(ServiceUri);
-			var originalColor = Console.ForegroundColor;
-						
-			Console.Write($"Ping: ");
-
-			if (string.IsNullOrEmpty(result))
-			{
-				Console.ForegroundColor = ConsoleColor.Green;
-				Console.WriteLine("Ok");
-				Console.ForegroundColor = originalColor;
-			}
-			else
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine("ERROR");
-				Console.ForegroundColor = originalColor;
-				Console.WriteLine("-------------");
-				Console.WriteLine(result);
-			}
+			ServicePath = options.Endpoint;
+			Console.WriteLine($"Ping {ServiceUri} ...");
+			return base.Execute(options);
 		}
 
 
