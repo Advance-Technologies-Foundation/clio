d39ac9dfc01871f32bffddc708aaa7e840e2690f
P.Makarchuk <P.Makarchuk@creatio.com>
2021-11-30 22:16:34 +0200

    Extended options for the command to install packages in a web application ("push-pkg", "install")

--- clio/Command/PushPackageCommand.cs
+++ clio/Command/PushPackageCommand.cs
@@ -17,6 +17,28 @@ namespace Clio.Command
 
 		[Option('r', "ReportPath", Required = false, HelpText = "Log file path")]
 		public string ReportPath { get; set; }
+
+		[Option("InstallSqlScript", Required = false, HelpText = "Install sql script")]
+		public bool? InstallSqlScript { get; set; }
+
+		[Option("InstallPackageData", Required = false, HelpText = "Install package data")]
+		public bool? InstallPackageData { get; set; }
+
+		[Option("ContinueIfError", Required = false, HelpText = "Continue if error")]
+		public bool? ContinueIfError { get; set; }
+
+		[Option("SkipConstraints", Required = false, HelpText = "Skip constraints")]
+		public bool? SkipConstraints { get; set; }
+
+		[Option("SkipValidateActions", Required = false, HelpText = "Skip validate actions")]
+		public bool? SkipValidateActions { get; set; }
+
+		[Option("ExecuteValidateActions", Required = false, HelpText = "Execute validate actions")]
+		public bool? ExecuteValidateActions { get; set; }
+
+		[Option("IsForceUpdateAllColumns", Required = false, HelpText = "Is force update all columns")]
+		public bool? IsForceUpdateAllColumns { get; set; }
+
 	}
 
 	public class PushPackageCommand : Command<PushPkgOptions>
@@ -27,10 +49,27 @@ namespace Clio.Command
 
 		}
 		private readonly IPackageInstaller _packageInstaller;
+		private readonly PackageInstallOptions _packageInstallOptionsDefault = new PackageInstallOptions();
+
+		private PackageInstallOptions ExtractPackageInstallOptions(PushPkgOptions options) {
+				var packageInstallOptions = new PackageInstallOptions {
+					InstallSqlScript = options.InstallSqlScript ?? true,
+					InstallPackageData = options.InstallPackageData ?? true,
+					ContinueIfError = options.ContinueIfError ?? true,
+					SkipConstraints = options.SkipConstraints ?? false,
+					SkipValidateActions = options.SkipValidateActions ?? false,
+					ExecuteValidateActions = options.ExecuteValidateActions ?? false,
+					IsForceUpdateAllColumns = options.IsForceUpdateAllColumns ?? false
+				};
+				return packageInstallOptions == _packageInstallOptionsDefault
+					? null
+					: packageInstallOptions;
+		}
 
 		public override int Execute(PushPkgOptions options) {
 			try {
-				bool success = _packageInstaller.Install(options.Name, options.ReportPath);
+				PackageInstallOptions packageInstallOptions = ExtractPackageInstallOptions(options);
+				bool success = _packageInstaller.Install(options.Name, packageInstallOptions, options.ReportPath);
 				Console.WriteLine(success ? "Done" : "Error");
 				return success ? 0 : 1;
 			} catch (Exception e) {
