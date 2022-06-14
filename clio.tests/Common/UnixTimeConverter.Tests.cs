78f6fea4194357e708ccc224d0e88646eae33db7
Kirill Krylov <k.krylov@creatio.com>
2022-06-15 00:18:08 +0300

    uncemmented test that failed on github runner

--- clio.tests/Common/UnixTimeConverter.Tests.cs
+++ clio.tests/Common/UnixTimeConverter.Tests.cs
@@ -13,21 +13,21 @@ namespace Clio.Tests.Common
 
 		#region Methods: Public
 
-		// [Test, Category("Unit")]
-		// public void CovertFromUnixDateTime_ReturnsDateTime() {
-		// 	const long unixDateTime = 1556958281234;
-		// 	DateTime expectedDateTime = new DateTime(2019, 5, 4, 11,24, 41, 234);
-		// 	DateTime actualDateTime = UnixTimeConverter.CovertFromUnixDateTime(unixDateTime);
-		// 	actualDateTime.Should().Be(expectedDateTime);
-		// }
+		[Test, Category("Unit")]
+		public void CovertFromUnixDateTime_ReturnsDateTime() {
+			const long unixDateTime = 1556958281234;
+			DateTime expectedDateTime = new DateTime(2019, 5, 4, 11,24, 41, 234);
+			DateTime actualDateTime = UnixTimeConverter.CovertFromUnixDateTime(unixDateTime);
+			actualDateTime.Should().Be(expectedDateTime);
+		}
 
-		// [Test, Category("Unit")]
-		// public void CovertToUnixDateTime_ReturnsUnixDateTime() {
-		// 	const long expectedUnixDateTime = 1556958281234;
-		// 	DateTime dateTime = new DateTime(2019, 5, 4, 11,24, 41, 234);
-		// 	long actualUnixDateTime = UnixTimeConverter.CovertToUnixDateTime(dateTime);
-		// 	actualUnixDateTime.Should().Be(expectedUnixDateTime);
-		// }
+		[Test, Category("Unit")]
+		public void CovertToUnixDateTime_ReturnsUnixDateTime() {
+			const long expectedUnixDateTime = 1556958281234;
+			DateTime dateTime = new DateTime(2019, 5, 4, 11,24, 41, 234);
+			long actualUnixDateTime = UnixTimeConverter.CovertToUnixDateTime(dateTime);
+			actualUnixDateTime.Should().Be(expectedUnixDateTime);
+		}
 
 		#endregion
 
