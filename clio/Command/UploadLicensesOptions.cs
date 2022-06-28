8e71edf5641cdded75e620e32e18afb2c921e2e7
P.Shvedun <P.Shvedun@creatio.com>
2022-06-29 00:44:39 +0300

    Added Upload Licenses Command

---
+++ clio/Command/UploadLicensesOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace Clio.Command
+{
+	[Verb("upload-licenses", Aliases = new string[] { "lic" }, HelpText = "Upload licenses")]
+	public class UploadLicensesOptions : EnvironmentOptions
+	{
+		[Value(0, MetaName = "FilePath", Required = true, HelpText = "License file path")]
+		public string FilePath { get; set; }
+	}
+}
