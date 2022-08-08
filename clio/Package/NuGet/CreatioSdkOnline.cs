c2255e9dcfea872a76441334e95d55d3d59f17e7
Kirill Krylov <k.krylov@creatio.com>
2022-08-08 07:59:02 +0300

    minor fix

--- clio/Package/NuGet/CreatioSdkOnline.cs
+++ clio/Package/NuGet/CreatioSdkOnline.cs
@@ -1,9 +1,7 @@
-﻿using Clio.Project.NuGet;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
