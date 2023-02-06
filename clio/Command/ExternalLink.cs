994fa8963638724857bf18a8a7fe497536471e01
Kirill Krylov <k.krylov@creatio.com>
2023-02-06 11:58:27 +0200

    updated comments

--- clio/Command/ExternalLink.cs
+++ clio/Command/ExternalLink.cs
@@ -29,12 +29,10 @@ namespace Clio.Command
 
 	public class ExternalLinkCommand : Command<ExternalLinkOptions>
 	{
-		private readonly RegAppCommand _regCommand;
 
 		#region Fields: Private
-
-
-
+		private readonly RegAppCommand _regCommand;
+		private Uri _clioUri;
 		#endregion
 
 		#region Constructors: Public
@@ -48,11 +46,25 @@ namespace Clio.Command
 
 		#region Methods: Public
 
+		/// <summary>
+		/// to test execute in command line
+		/// clio-dev externalLink --content clio://?protocol=https:&host=129117-crm-bundle.creatio.com&name=vscode&clientId=83B03D807E3DEEAEF6A55D8CB587E191&clientSecret=C6EA75A49446A63F239BEB4C89892A610E638063AC298EEAF6786E309E06970C
+		/// </summary>
+		/// <param name="options"></param>
+		/// <returns></returns>
 		public override int Execute(ExternalLinkOptions options)
 		{
 
-			Uri uri = new Uri(options.Content);
-			NameValueCollection clioParams = System.Web.HttpUtility.ParseQueryString(uri.Query);
+			if (Uri.TryCreate(options.Content, UriKind.Absolute, out _clioUri))
+			{
+				if (_clioUri.Scheme != "clio")
+				{
+					Console.Error.WriteLine("ERROR - Not a clio URI");
+					return 0;
+				}
+			}
+
+			NameValueCollection clioParams = System.Web.HttpUtility.ParseQueryString(_clioUri.Query);
 
 			Console.WriteLine("clio was called with:");
 			for (var i = 0; i < clioParams.Count; i++)
@@ -70,11 +82,11 @@ namespace Clio.Command
 
 			var opt = new RegAppOptions
 			{
-				IsNetCore = false,                                      //Should pass in deepLink as arg
+				IsNetCore = false,                                      //Should pass in deepLink as arg, in JS check if this.window.location.pathname starts with /0
 				ClientId = clioParams["clientId"],
 				ClientSecret = clioParams["clientSecret"],
 				Uri = baseUrl,
-				Name = clioParams["name"],
+				Name = clioParams["name"],                          //Probably needs a unique name
 				AuthAppUri = authUrl,
 				Login = string.Empty,
 				Password = string.Empty,
@@ -82,7 +94,6 @@ namespace Clio.Command
 			};
 
 			_regCommand.Execute(opt);
-
 
 			Console.ReadLine();
 			return 0;
