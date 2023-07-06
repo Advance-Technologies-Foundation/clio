98b361b9386c9719e38e7ea53fc028869ce80875
Kyryl Krylov, CPA <k.krylov@creatio.com>
2023-07-06 23:44:23 +0300

    Refactor (#239)

---
+++ clio.tests/Common/ArtifactFilterTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Clio.Tests.Common;
+
+using Clio.Common;
+using NUnit.Framework;
+
+[TestFixture, Author("Kirill Krylov")]
+public class ArtifactFilterTests
+{
+	public ArtifactFilter sut;
+	const string _baseDirectory = "\\\\tscrm.com\\dfs-ts\\builds-7";
+	
+	[SetUp]
+	protected void SetUp()
+	{
+		sut = new ArtifactFilter(_baseDirectory);
+	}
+
+	[Test]
+	public void GetDirectories_Returns_Directories()
+	{
+		var result = ArtifactFilter.GetDirectories(_baseDirectory);
+		Assert.That(result, Is.Not.Null);
+	}
+}
