b5f50186ac4c6290d6b004883688bd35569999e2
Kirill Krylov <k.krylov@creatio.com>
2023-07-06 23:43:03 +0300

    Refactor

---
+++ clio/Common/ArtifactFilter.cs
@@ -0,0 +1,27 @@
+﻿namespace Clio.Common;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+public class ArtifactFilter
+{
+
+	private readonly string _baseDirectory;
+	
+	public ArtifactFilter(string baseDirectory) {
+		_baseDirectory = baseDirectory;
+	}
+	
+	internal static readonly Func<string, IEnumerable<string>> GetDirectories = (baseDirectory) => {
+		
+		var mylist = Directory.GetDirectories(baseDirectory)
+			.Select(item=> (new Uri(item, UriKind.Absolute)).Segments.Last())
+			.Where(item=> item==baseDirectory);
+		
+		return mylist;
+	};
+
+}
