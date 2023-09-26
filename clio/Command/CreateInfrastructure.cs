c21450fff5d4228105acb1b7daa96e5a3c5c7072
Kirill Krylov <k.krylov@creatio.com>
2023-09-26 03:13:31 +0300

    changed text in command

--- clio/Command/CreateInfrastructure.cs
+++ clio/Command/CreateInfrastructure.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
@@ -33,18 +34,37 @@ namespace Clio.Command
 			_fileSystem.CopyDirectory(from,to, true);
 
 			var color = Console.ForegroundColor;
-			//Console.ForegroundColor = ;
-			Console.WriteLine("**********************  IMPORTANT **********************");
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.WriteLine("****************************  IMPORTANT ****************************");
+			Console.ForegroundColor = color;
 			Console.WriteLine($"All files have been copied to:");
 			Console.WriteLine($"\t{to}");
 			Console.WriteLine();
 			Console.ForegroundColor = ConsoleColor.DarkRed;
 			Console.WriteLine("1. Make sure to review files and change values if needed");
 			Console.WriteLine("2. If you have more than one cluster configured, make sure to switch to Rancher Desktop");
+			Console.WriteLine();
+
+			Console.ForegroundColor = color;
+			Console.WriteLine("Files Include:");
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			IList<string[]> table = new List<string[]>();
+			table.Add(new []{"Application","Version","Available on"});
+			table.Add(new []{"-------------------------","------------------------","------------"});
+			table.Add(new []{"Postgres SQL Server","latest","Port: 5432"});
+			table.Add(new []{"Microsoft SQL Server 2022","latest developer edition","Port: 1434"});
+			table.Add(new []{"Redis Server","latest", "Port: 6379"});
+			table.Add(new []{"Email Listener","1.0.10", "Port: 1090"});
+			
+			Console.Write(TextUtilities.ConvertTableToString(table));
+			Console.WriteLine();
 			
 			Console.ForegroundColor = color;
 			Console.WriteLine();
+			Console.ForegroundColor = ConsoleColor.DarkRed;
 			Console.WriteLine("Clio will not deploy infrastructure automatically");
+			Console.WriteLine();
+			Console.ForegroundColor = color;
 			Console.WriteLine($"To deploy new infrastructure execute from {to} folder in any terminal:");
 			Console.ForegroundColor = ConsoleColor.DarkYellow;
 			Console.WriteLine($"\tkubectl apply -f infrastructure");
