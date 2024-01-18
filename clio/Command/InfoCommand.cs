1e2510bbab491507176abe6178b73d98ea555170
Kirill Krylov <k.krylov@creatio.com>
2024-01-18 21:57:04 +0200

    InfoCommand
    Signed-off-by: Kirill Krylov <k.krylov@creatio.com>

--- clio/Command/InfoCommand.cs
+++ clio/Command/InfoCommand.cs
@@ -4,11 +4,17 @@ using System.Reflection;
 
 namespace Clio.Command
 {
-	[Verb("ver", Aliases = new string[] { "get-version" }, HelpText = "Check for Creatio packages updates in NuGet")]
-	public class GetVersionOptions
+	[Verb("info", Aliases = new string[] { "ver","get-version","i" }, HelpText = "Check for Creatio packages updates in NuGet")]
+	public class InfoCommandOptions
 	{
 		[Option("all", Required = false, HelpText = "Get versions for all known components")]
 		public bool All
+		{
+			get; set;
+		}
+		
+		[Option('s', "settings-file",  Required = false, HelpText = "Get path to settings file")]
+		public bool ShowSettingsFilePath
 		{
 			get; set;
 		}
@@ -32,10 +38,10 @@ namespace Clio.Command
 		}
 	}
 
-	public class GetVersionCommand : Command<GetVersionOptions>
+	public class InfoCommand : Command<InfoCommandOptions>
 	{
 		private const string _gateVersion = "2.0.0.26";
-		public override int Execute(GetVersionOptions options)
+		public override int Execute(InfoCommandOptions options)
 		{
 			if (options is object && options.Clio)
 			{
@@ -54,10 +60,14 @@ namespace Clio.Command
 			}
 			else if (options is object && options.All || (!options.Runtime && !options.Gate && !options.Clio))
 			{
-				Console.WriteLine("clio:   {0}", Assembly.GetEntryAssembly().GetName().Version);
-				Console.WriteLine("gate:   {0}", _gateVersion);
-				Console.WriteLine("dotnet: {0}", Environment.Version.ToString());
+				Console.WriteLine("clio:               {0}", Assembly.GetEntryAssembly().GetName().Version);
+				Console.WriteLine("gate:               {0}", _gateVersion);
+				Console.WriteLine("dotnet:             {0}", Environment.Version.ToString());
+				Console.WriteLine("settings file path: {0}", SettingsRepository.AppSettingsFile);
 				return 0;
+			}
+			else if(options.ShowSettingsFilePath) {
+				Console.WriteLine(SettingsRepository.AppSettingsFile);
 			}
 			return 1;
 		}
