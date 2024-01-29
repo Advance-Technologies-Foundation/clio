e38eaa604b28000f610baa97b365aa1c07cc6f0b
Vadym Artemchuk <v.artemchuk@creatio.com>
2024-01-29 11:35:04 +0200

    execute-assembly-code command added test

---
+++ clio.tests/Command/AssemblyCommandTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Clio.Tests.Command;
+
+using System;
+using System.IO;
+using System.Text.Json;
+using Clio.Command;
+using Clio.Common;
+using NSubstitute;
+using NUnit.Framework;
+
+[TestFixture]
+public class AssemblyCommandTestCase
+{
+
+	private TextWriter _defaultConsoleWriter;
+
+	private bool CheckRequest(string body, string executorType) {
+		try {
+			var reqObj = JsonSerializer.Deserialize<ExecuteScriptRequest>(body);
+			return reqObj.LibraryType == executorType && reqObj.Body.Length > 0;
+		} catch (Exception) {
+			return false;
+		}
+	}
+
+	[SetUp]
+	protected void Setup() {
+		_defaultConsoleWriter = Console.Out;
+	}
+
+	[TearDown]
+	protected void TearDown() {
+		Console.SetOut(_defaultConsoleWriter);
+	}
+
+	[Test, Category("Unit")]
+	public void Execute_ShouldWriteResponse_WhenItIsSuccessful() {
+		var applicationClient = Substitute.For<IApplicationClient>();
+		var command = new AssemblyCommand(applicationClient, new EnvironmentSettings());
+		string executorType = typeof(AssemblyCommand).FullName;
+		applicationClient.ExecutePostRequest(Arg.Is<string>(path => path.EndsWith("/IDE/ExecuteScript")),
+			Arg.Is<string>(request => CheckRequest(request, executorType)))
+			.Returns("responseFromServer");
+		var output = new StringWriter();
+		Console.SetOut(output);
+		command.Execute(new ExecuteAssemblyOptions {
+			ExecutorType = executorType,
+			Name = new Uri(typeof(AssemblyCommand).Assembly.Location).LocalPath,
+			WriteResponse = true
+		});
+		StringAssert.StartsWith("responseFromServer", output.ToString());
+	}
+
+}
--- clio/Command/AssemblyCommand.cs
+++ clio/Command/AssemblyCommand.cs
@@ -5,6 +5,8 @@ using CommandLine;
 
 namespace Clio.Command
 {
+	using System.Text.Json;
+
 	[Verb("execute-assembly-code",
 		Aliases = new[] { "exec" },
 		HelpText = "Execute an assembly code which implements the IExecutor interface",
@@ -21,23 +23,28 @@ namespace Clio.Command
 		public bool WriteResponse { get; set; }
 	}
 
+	internal class ExecuteScriptRequest
+	{
+		public byte[] Body { get; set; }
+		public string LibraryType { get; set; }
+	}
+
 	class AssemblyCommand : RemoteCommand<ExecuteAssemblyOptions>
 	{
-		private ExecuteAssemblyOptions _options;
 		protected override string ServicePath => @"/IDE/ExecuteScript";
-
 
 		public AssemblyCommand(IApplicationClient applicationClient, EnvironmentSettings settings)
 			: base(applicationClient, settings) {
 		}
 
 		protected override string GetRequestData(ExecuteAssemblyOptions options) {
-			_options = options;
 			string filePath = options.Name;
 			string executorType = options.ExecutorType;
 			var fileContent = File.ReadAllBytes(filePath);
-			string body = Convert.ToBase64String(fileContent);
-			return @"{""Body"":""" + body + @""",""LibraryType"":""" + executorType + @"""}";
+			return JsonSerializer.Serialize(new ExecuteScriptRequest {
+				Body = fileContent,
+				LibraryType = executorType
+			});
 		}
 
 		protected override void ProceedResponse(string response, ExecuteAssemblyOptions options) {
