620d0005a2238a601e10ca9da478560e05e1e306
Vladimir <nikonov.volodymyr@gmail.com>
2024-02-01 21:21:03 +0100

    add option to lia command for output in json format
    clio lia --json

--- clio/Command/ListInstalledApplications.cs
+++ clio/Command/ListInstalledApplications.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using ATF.Repository;
 using ATF.Repository.Providers;
 using Clio.Common;
@@ -11,7 +12,10 @@ namespace Clio.Command;
 [Verb("get-app-list", Aliases = new[] {"lia", "list-apps", "apps", "app-list", "apps-list", "list - installed - applications" },
 	HelpText = "List installed apps")]
 public class ListInstalledAppsOptions : EnvironmentOptions
-{ }
+{
+	[Option("json", Required = false, HelpText = "Use json format for output", Default = false)]
+	public bool JsonFormat { get; set; }
+}
 
 public class ListInstalledAppsCommand : Command<ListInstalledAppsOptions>
 {
@@ -39,12 +43,17 @@ public class ListInstalledAppsCommand : Command<ListInstalledAppsOptions>
 		table.Columns.Add(nameof(SysInstalledApp.Name));
 		table.Columns.Add(nameof(SysInstalledApp.Code));
 		table.Columns.Add(nameof(SysInstalledApp.Version));
-
-		AppDataContextFactory.GetAppDataContext(_provider)
+		var applications = AppDataContextFactory.GetAppDataContext(_provider)
 			.Models<SysInstalledApp>()
-			.ToList()
-			.ForEach(m => { table.AddRow(m.Name, m.Code, m.Version); });
-		_logger.PrintTable(table);
+			.ToList();
+		if (!options.JsonFormat) {
+			applications.ForEach(m => { table.AddRow(m.Name, m.Code, m.Version); });
+			_logger.PrintTable(table);
+		} else {
+			_logger.Write(JsonSerializer.Serialize(applications, new JsonSerializerOptions {
+				WriteIndented = true
+			}));
+		}
 		return 0;
 	}
 
