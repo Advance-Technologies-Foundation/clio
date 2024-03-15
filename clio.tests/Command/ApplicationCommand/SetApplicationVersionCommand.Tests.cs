c9cf7b3c2c37c8382ee38429a5f7c9855b30e9cd
Pavel Shvedun <P.Shvedun@creatio.com>
2024-03-15 03:34:04 +0200

    Added test for SetApplicationVersionCommand

--- clio.tests/Command/ApplicationCommand/SetApplicationVersionCommand.Tests.cs
+++ clio.tests/Command/ApplicationCommand/SetApplicationVersionCommand.Tests.cs
@@ -12,7 +12,7 @@ namespace Clio.Tests.Command.ApplicationCommand
 	internal class SetApplicationVersionCommandTest: BaseCommandTests<SetApplicationVersionOption>
 	{
 
-		private static string mockWorspacePath = Path.Combine("C:", "iframe-sample");
+		private static string mockWorspacePath = Path.Combine("C:", "MockWorkspaceFolder");
 		private static string appDescriptorJsonPath = Path.Combine(mockWorspacePath, "packages", "IFrameSample", "Files", "app-descriptor.json");
 
 		private static MockFileSystem CreateFs(string filePath) {
@@ -27,22 +27,82 @@ namespace Clio.Tests.Command.ApplicationCommand
 			});
 		}
 
+		private static MockFileSystem CreateFs(Dictionary<string, string> appDescriptors) {
+			string originClioSourcePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+			MockFileSystem mockFileSystem = new MockFileSystem();
+			foreach (var appDescriptor in appDescriptors) {
+				string appDescriptorExamplesDescriptorPath = Path.Combine(originClioSourcePath, "Examples", "AppDescriptors", appDescriptor.Value);
+				string mockAppDescriptorJsonPath = Path.Combine(mockWorspacePath, "packages", appDescriptor.Key, "Files", "app-descriptor.json");
+				mockFileSystem.AddFile(mockAppDescriptorJsonPath, new MockFileData(File.ReadAllText(appDescriptorExamplesDescriptorPath)));
+			}
+			return mockFileSystem;
+		}
+
 		private MockFileSystem _fileSystem;
 
-		[TestCase("app-descriptor.json")]
+		[TestCase("app-descriptor_v.json")]
 		[TestCase("app-descriptor_wv.json")]
 		[TestCase("app-descriptor_dv.json")]
-		public void SetVersion_WhenSetCorrectVersion(string descriptorPath) {
+		public void SetVersion_WhenWorkspaceContainsOneApplication(string descriptorPath) {
 			_fileSystem = CreateFs(descriptorPath);
 			string expectedVersion = "8.1.1";
 			var command = new SetApplicationVersionCommand(_fileSystem);
 			string worspaceFolderPath = mockWorspacePath;
-			command.Execute(new SetApplicationVersionOption() { Version = expectedVersion, WorspaceFolderPath = worspaceFolderPath });
+			command.Execute(new SetApplicationVersionOption() {
+				Version = expectedVersion, WorspaceFolderPath = worspaceFolderPath
+			});
 			var objectJson = JsonObject.Parse(_fileSystem.File.ReadAllText(appDescriptorJsonPath));
 			string actualVersion = objectJson["Version"];
 			Assert.True(_fileSystem.FileExists(appDescriptorJsonPath));
 			Assert.AreEqual(expectedVersion, actualVersion);
 			Assert.Greater(20, _fileSystem.File.ReadAllLines(appDescriptorJsonPath).Length);
+		}
+
+		[Test]
+		public void SetVersion_ThrowException_WhenWorkspaceContainsMoreThanOneApplication() {
+			Dictionary<string, string> appDescriptions = new Dictionary<string, string>();
+			appDescriptions.Add("Package1", "app1-app-descriptor.json");
+			appDescriptions.Add("Package2", "app2-app-descriptor.json");
+			_fileSystem = CreateFs(appDescriptions);
+			string expectedVersion = "8.1.1";
+			var command = new SetApplicationVersionCommand(_fileSystem);
+			string worspaceFolderPath = mockWorspacePath;
+			var exception = Assert.Throws<Exception>( () => command.Execute(new SetApplicationVersionOption() { 
+				Version = expectedVersion, WorspaceFolderPath = worspaceFolderPath }));
+			Assert.True(exception.Message.Contains("Package1"));
+			Assert.True(exception.Message.Contains("Package2"));
+		}
+
+		[Test]
+		public void SetVersion_ThrowExceptionWhenAplicationExtendedAndPackageNotDefined() {
+			Dictionary<string, string> appDescriptions = new Dictionary<string, string>();
+			appDescriptions.Add("Package1", "app1-app-descriptor.json");
+			appDescriptions.Add("Package2", "app1-ext-app-descriptor.json");
+			_fileSystem = CreateFs(appDescriptions);
+			string expectedVersion = "8.1.1";
+			var command = new SetApplicationVersionCommand(_fileSystem);
+			string worspaceFolderPath = mockWorspacePath;
+			var exception = Assert.Throws<Exception>(() => command.Execute(new SetApplicationVersionOption() { 
+				Version = expectedVersion, WorspaceFolderPath = worspaceFolderPath }));
+			Assert.True(exception.Message.Contains("Package1"));
+			Assert.True(exception.Message.Contains("Package2"));
+		}
+
+		[Test]
+		public void SetVersion_WhenAplicationExtendedAndPackageDefined() {
+			Dictionary<string, string> appDescriptions = new Dictionary<string, string>();
+			string extendPackageName = "Package2";
+			appDescriptions.Add("Package1", "app1-app-descriptor.json");
+			appDescriptions.Add(extendPackageName, "app1-ext-app-descriptor.json");
+			_fileSystem = CreateFs(appDescriptions);
+			string expectedVersion = "8.1.1";
+			var command = new SetApplicationVersionCommand(_fileSystem);
+			string worspaceFolderPath = mockWorspacePath;
+			command.Execute(new SetApplicationVersionOption() {
+				Version = expectedVersion,
+				WorspaceFolderPath = worspaceFolderPath,
+				PackageName = extendPackageName
+			});
 		}
 
 	}
--- clio/Command/ApplicationCommand/SetApplicationVersionCommand.cs
+++ clio/Command/ApplicationCommand/SetApplicationVersionCommand.cs
@@ -1,7 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
 using System.Json;
+using System.Text;
 using CommandLine;
+using Terrasoft.Common;
 
 namespace Clio.Command.ApplicationCommand
 {
@@ -22,6 +26,13 @@ namespace Clio.Command.ApplicationCommand
 			get;
 			internal set;
 		}
+
+		[Option('p', "PackageName", Required = false, HelpText = "Package name")]
+		public string PackageName
+		{
+			get;
+			internal set;
+		}
 	}
 
 	internal class SetApplicationVersionCommand : Command<SetApplicationVersionOption>
@@ -35,6 +46,30 @@ namespace Clio.Command.ApplicationCommand
         public override int Execute(SetApplicationVersionOption options) {
 			string packagesFolderPath = Path.Combine(options.WorspaceFolderPath, "packages");
 			string[] appDescriptorPaths = fileSystem.Directory.GetFiles(packagesFolderPath, "app-descriptor.json", SearchOption.AllDirectories);
+			if (appDescriptorPaths.Length > 1) {
+				string code = string.Empty;
+				foreach (var descriptor in appDescriptorPaths) {
+					string actualCode = JsonObject.Parse(fileSystem.File.ReadAllText(descriptor))["Code"].ToString();
+					if (code != actualCode && code != string.Empty) {
+						StringBuilder exceptionMessage = new StringBuilder();
+						exceptionMessage.AppendLine("Find more than one applications: ");
+						foreach (var path in appDescriptorPaths) {
+							exceptionMessage.AppendLine(path);
+						}
+						throw new Exception(exceptionMessage.ToString());
+					} else {
+						code = actualCode;
+					}
+				}
+				if (options.PackageName.IsNullOrEmpty()) {
+					StringBuilder exceptionMessage = new StringBuilder();
+					exceptionMessage.AppendLine($"Find more than one descriptors for application {code}. Specify package name.");
+					foreach (var path in appDescriptorPaths) {
+						exceptionMessage.AppendLine(path);
+					}
+					throw new Exception(exceptionMessage.ToString());
+				}
+			}
 			string appDescriptorPath = appDescriptorPaths[0];
 			var objectJson = JsonObject.Parse(fileSystem.File.ReadAllText(appDescriptorPath));
 			objectJson["Version"] = options.Version;
