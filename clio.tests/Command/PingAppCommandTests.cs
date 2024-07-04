e8abaab8e06bcf96b8e90a10b756f860112b1c0a
Vladimir <nikonov.volodymyr@gmail.com>
2024-07-04 23:53:59 +0200

    delete creatio.client.csproj

---
+++ clio.tests/Command/PingAppCommandTests.cs
@@ -0,0 +1,16 @@
+﻿using Clio.Command;
+using NUnit.Framework;
+
+namespace Clio.Tests.Command
+{
+
+	[TestFixture]
+	internal class PingAppCommandTests : BaseCommandTests<PingAppOptions>
+	{
+		[Test]
+		public void PingAppCommandShoulBeUsesAllRetryOptions() {
+			
+		}
+	}
+
+}
