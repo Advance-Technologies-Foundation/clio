16c8cf465766c4198443a27d4f0873ceffef26be
Vladimir <nikonov.volodymyr@gmail.com>
2024-07-12 01:09:11 +0200

    add mock data command

---
+++ clio.tests/Command/MockDataCommandTests.cs
@@ -0,0 +1,53 @@
+﻿using Clio.Command;
+using Clio.Common;
+using Clio.Tests.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clio.Tests.Command
+{
+	[TestFixture]
+	internal class MockDataCommandTests : BaseCommandTests<MockDataCommandOptions>
+	{
+		protected override MockFileSystem CreateFs() {
+			var mockFS = base.CreateFs();
+			mockFS.MockExamplesFolder("MockDataProjects", @"T:\MockDataProjects");
+			return mockFS;
+		}
+
+		[Test]
+		public void CreateDataFiles() {
+			var clioFileSystem = new FileSystem(_fileSystem);
+			// Arrange
+			var command = new MockDataCommand(clioFileSystem);
+			var options = new MockDataCommandOptions() {
+				Models = @"T:\MockDataProjects",
+				Data = @"T:\MockDataProjects\Tests\MockData"
+			};
+			command.Execute(options);
+			_fileSystem.Directory.Exists(options.Models).Should().BeTrue();
+			_fileSystem.Directory.Exists(options.Data).Should().BeTrue();
+		}
+
+		[Test]
+		public void FindModelsp() {
+			// Arrange
+			var clioFileSystem = new FileSystem(_fileSystem);
+			var command = new MockDataCommand(clioFileSystem);
+			var options = new MockDataCommandOptions() {
+				Models = @"T:\MockDataProjects",
+				Data = @"T:\MockDataProjects\Tests\MockData"
+			};
+			var models = command.FindModels(options.Models);
+			models.Count.Should().Be(3);
+			models.Should().Contain("Contact");
+			models.Should().Contain("Account");
+		}
+	}
+}
---
+++ clio/Command/MockDataCommand.cs
@@ -0,0 +1,59 @@
+﻿using Clio.Common;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Clio.Command
+{
+	[Verb("mock-data", Aliases = new string[] { "data-mock" }, HelpText = "Build package command")]
+	public class MockDataCommandOptions : RemoteCommandOptions
+	{
+		public string Models { get; internal set; }
+		public string Data { get; internal set; }
+	}
+
+	internal class MockDataCommand : RemoteCommand<MockDataCommandOptions>
+	{
+		private readonly IFileSystem _fileSystem;
+
+		public MockDataCommand(IApplicationClient applicationClient, EnvironmentSettings environmentSettings, FileSystem fileSystem) : base(applicationClient, environmentSettings) {
+			this._fileSystem = fileSystem;
+		}
+
+		public MockDataCommand(IFileSystem fileSystem) {
+			this._fileSystem = fileSystem;
+		}
+
+		public override int Execute(MockDataCommandOptions options) {
+			return base.Execute(options);
+		}
+
+		internal List<string> FindModels(string models) {
+			List<string> schemaNames = new List<string>();
+			var files = _fileSystem.GetFiles(models, "*.*", System.IO.SearchOption.AllDirectories).ToList();
+			foreach (var file in files) {
+				var fileContent = _fileSystem.ReadAllText(file);
+				schemaNames.AddRange(ExtractSchemaNames(fileContent));
+			}
+			return schemaNames.Distinct().ToList();
+		}
+
+		public static List<string> ExtractSchemaNames(string sourceCode) {
+			List<string> schemaNames = new List<string>();
+			string pattern = @"\[Schema\(""([^""]+)""\)\]";
+			MatchCollection matches = Regex.Matches(sourceCode, pattern);
+
+			foreach (Match match in matches) {
+				if (match.Groups.Count > 1) {
+					schemaNames.Add(match.Groups[1].Value);
+				}
+			}
+
+			return schemaNames;
+		}
+	}
+}
