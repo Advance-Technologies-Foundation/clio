bb79cdee917466bbbfbc0b5b3a69e328fe0e9cef
Vladimir <nikonov.volodymyr@gmail.com>
2024-09-26 21:53:35 +0200

    Update K8SCommandTests.cs

--- clio.tests/Command/K8SCommandTests.cs
+++ clio.tests/Command/K8SCommandTests.cs
@@ -3,30 +3,26 @@ using Clio.Common.K8;
 using FluentAssertions;
 using k8s;
 using k8s.Exceptions;
-using NSubstitute;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Clio.Tests.Command
 {
 	[TestFixture]
-	internal class K8SCommandTests: BaseClioModuleTests
+	internal class K8SCommandTests : BaseClioModuleTests
 	{
 		override protected void AdditionalRegistrations(ContainerBuilder containerBuilder) {
 			base.AdditionalRegistrations(containerBuilder);
 			containerBuilder.Register<Kubernetes>(provider => {
-				throw new KubernetesException("Kubernetes cannot be recognized.");
+				throw new KubeConfigException("Kubernetes cannot be recognized.");
 			}).As<IKubernetes>();
 		}
 
 		[Test]
 		public void CreateK8SCommand_shouldthrowexception_ifKubernetesCannotBeRecognized() {
 			Action act = () => Container.Resolve<k8Commands>();
-			act.Should().Throw<Exception>().WithInnerException<KubernetesException>()
+			act.Should().Throw<Exception>().WithInnerException<KubeConfigException>()
 				.Which.Message.Should().BeEquivalentTo("Kubernetes cannot be recognized.");
 		}
 	}
