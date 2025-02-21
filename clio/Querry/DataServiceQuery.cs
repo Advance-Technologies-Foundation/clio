038be57f05f8fd33e6674a6b2ade435d1888110e
Kirill Krylov <k.krylov@creatio.com>
2025-02-21 01:33:06 +0100

    Refactor DataServiceQuery and CallServiceCommand for improved option handling and JSON beautification

--- clio/Querry/DataServiceQuery.cs
+++ clio/Querry/DataServiceQuery.cs
@@ -1,153 +1,185 @@
 ﻿using System;
-using Clio.Command;
-using Clio.Common;
-using CommandLine;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using Clio;
-using Clio.Querry;
+using Clio.Command;
+using Clio.Common;
+using CommandLine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace Clio.Querry
-{
-    [Verb("call-service", Aliases = new[] { "cs" }, HelpText = "Call Service Request")]
-    public class CallServiceCommandOptions : RemoteCommandOptions
-    {
-        [Option('f', "input", Required = true, HelpText = "Request file", Separator = ' ')]
-        public string ReqeustFileName { get; set; }
+namespace Clio.Querry;
 
-        [Option('d', "destination", Required = true, HelpText = "Destination set")]
-        public string ResultFileName { get; set; }
+[Verb("call-service", Aliases = new[] {"cs"}, HelpText = "Call Service Request")]
+public class CallServiceCommandOptions : RemoteCommandOptions {
 
-        [Option('v', "variables", Required = false, HelpText = "Result file", Separator = ';')]
-        public IEnumerable<string> Variables { get; set; }
+	#region Properties: Public
 
-        [Option("service-path", Required = false, HelpText = "Route service path")]
-        public string ServicePath { get; set; }
-    }
+	[Option('f', "input", Required = false, HelpText = "Request file", Separator = ' ')]
+	public string ReqeustFileName { get; set; }
 
-    [Verb("dataservice", Aliases = new[] { "ds" }, HelpText = "DataService Request")]
-    public class DataServiceQuerryOptions : CallServiceCommandOptions
-    {
-        [Option('t', "type", Required = true, HelpText = "Operation type", Separator = ' ')]
-        public string OperationType { get; set; }
-    }
-    
-    public class CallServiceCommand : BaseServiceCommand<CallServiceCommandOptions>
-    {
-        public CallServiceCommand(IApplicationClient applicationClient,
-            EnvironmentSettings settings,
-            IServiceUrlBuilder serviceUrlBuilder)
-            : base(applicationClient, settings, serviceUrlBuilder)
-        {
-        }
-    }
+	[Option('d', "destination", Required = false, HelpText = "Destination set")]
+	public string ResultFileName { get; set; }
 
-    public abstract class BaseServiceCommand<T> : RemoteCommand<T> where T : CallServiceCommandOptions
-    {
-        protected readonly IServiceUrlBuilder ServiceUrlBuilderInstance;
+	[Option("service-path", Required = false, HelpText = "Route service path")]
+	public string ServicePath { get; set; }
 
-        protected BaseServiceCommand(IApplicationClient applicationClient,
-            EnvironmentSettings settings,
-            IServiceUrlBuilder serviceUrlBuilderInstance)
-            : base(applicationClient, settings)
-        {
-            ServiceUrlBuilderInstance = serviceUrlBuilderInstance;
-        }
+	[Option('v', "variables", Required = false, HelpText = "Result file", Separator = ';')]
+	public IEnumerable<string> Variables { get; set; }
 
-        protected virtual string BuildUrl(T options)
-        {
-            return ServiceUrlBuilderInstance.Build(options.ServicePath);
-        }
+	#endregion
 
-        protected string GetRequestData(string requestFileName)
-        {
-            FileInfo fi = new FileInfo(requestFileName);
-            if (!fi.Exists)
-            {
-                throw new FileNotFoundException("File not found", requestFileName);
-            }
-            return File.ReadAllText(requestFileName);
-        }
+}
 
-        public override int Execute(T options)
-        {
-            string requestData = GetRequestData(options.ReqeustFileName);
-            if (options.Variables != null && options.Variables.Any())
-            {
-                requestData = ReplaceVariablesInJson(requestData, options.Variables);
-            }
-            ExecuteServiceRequest(BuildUrl(options), requestData, options.ResultFileName);
-            return 0;
-        }
+[Verb("dataservice", Aliases = new[] {"ds"}, HelpText = "DataService Request")]
+public class DataServiceQuerryOptions : CallServiceCommandOptions {
 
-        protected string ExecuteServiceRequest(string url, string requestData, string resultFileName = null)
-        {
-            string jsonResult = ApplicationClient.ExecutePostRequest(url, requestData);
+	#region Properties: Public
 
-            if (string.IsNullOrWhiteSpace(resultFileName))
-            {
-                Logger.WriteInfo(jsonResult);
-            }
-            else
-            {
-                string outputFileName = SetOutputFileName(resultFileName);
-                File.WriteAllText(outputFileName, jsonResult);
-            }
+	[Option('t', "type", Required = true, HelpText = "Operation type", Separator = ' ')]
+	public string OperationType { get; set; }
 
-            return jsonResult;
-        }
+	#endregion
 
-        protected string ReplaceVariablesInJson(string json, IEnumerable<string> variables)
-        {
-            if (variables == null)
-                return json;
+}
 
-            foreach (var variable in variables) {
-                var pattern = "{{" + variable.Split('=')[0] + "}}";
-                var regex = new Regex(pattern);
-                var match = regex.Match(json);
-                if (match.Success)
-                {
-                    json = regex.Replace(json, variable.Split('=')[1]);
-                }
-            }
-            return json;
-        }
+public class CallServiceCommand : BaseServiceCommand<CallServiceCommandOptions> {
 
-        protected string SetOutputFileName(string resultFileName)
-        {
-            FileInfo fi = new FileInfo(resultFileName);
-            string fileName = Path.GetFileNameWithoutExtension(resultFileName);
-            int count = fi.Directory.GetFiles($"{fileName}*{fi.Extension}").Count();
-            if (count > 0)
-            {
-                return Path.Combine(fi.Directory.ToString(), $"{fileName}-({count + 1}){fi.Extension}");
-            }
-            return Path.Combine(fi.Directory.ToString(), $"{fileName}{fi.Extension}");
-        }
-    }
+	#region Constructors: Public
 
-    public class DataServiceQuery : BaseServiceCommand<DataServiceQuerryOptions>
-    {
-        public DataServiceQuery(IApplicationClient applicationClient,
-            EnvironmentSettings settings,
-            IServiceUrlBuilder serviceUrlBuilder)
-            : base(applicationClient, settings, serviceUrlBuilder)
-        {
-        }
+	public CallServiceCommand(IApplicationClient applicationClient,
+		EnvironmentSettings settings,
+		IServiceUrlBuilder serviceUrlBuilder)
+		: base(applicationClient, settings, serviceUrlBuilder){ }
 
-        protected override string BuildUrl(DataServiceQuerryOptions options)
-        {
-            return options.OperationType.ToUpperInvariant() switch
-            {
-                "SELECT" => ServiceUrlBuilderInstance.Build(ServiceUrlBuilder.KnownRoute.Select),
-                "INSERT" => ServiceUrlBuilderInstance.Build(ServiceUrlBuilder.KnownRoute.Insert),
-                "UPDATE" => ServiceUrlBuilderInstance.Build(ServiceUrlBuilder.KnownRoute.Update),
-                "DELETE" => ServiceUrlBuilderInstance.Build(ServiceUrlBuilder.KnownRoute.Delete),
-                var _ => throw new Exception("Unknown operation type"),
-            };
-        }
-    }
+	#endregion
+
+}
+
+public abstract class BaseServiceCommand<T> : RemoteCommand<T> where T : CallServiceCommandOptions {
+
+	#region Fields: Protected
+
+	protected readonly IServiceUrlBuilder ServiceUrlBuilderInstance;
+
+	#endregion
+
+	#region Constructors: Protected
+
+	protected BaseServiceCommand(IApplicationClient applicationClient,
+		EnvironmentSettings settings,
+		IServiceUrlBuilder serviceUrlBuilderInstance)
+		: base(applicationClient, settings){
+		ServiceUrlBuilderInstance = serviceUrlBuilderInstance;
+	}
+
+	#endregion
+
+	#region Methods: Private
+
+	private string BeautifyJsonIfPossible(string input){
+		try {
+			JToken parsedJson = JToken.Parse(input);
+			return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+		}
+		catch (JsonReaderException) {
+			return input;
+		}
+	}
+
+	#endregion
+
+	#region Methods: Protected
+
+	protected virtual string BuildUrl(T options){
+		return ServiceUrlBuilderInstance.Build(options.ServicePath);
+	}
+
+	protected string ExecuteServiceRequest(string url, string requestData, string resultFileName = null){
+		string jsonResult = ApplicationClient.ExecutePostRequest(url, requestData);
+
+		string beautifiedJson = BeautifyJsonIfPossible(jsonResult);
+		if (string.IsNullOrWhiteSpace(resultFileName)) {
+			Logger.WriteLine(beautifiedJson);
+		}
+		else {
+			File.WriteAllText(resultFileName, beautifiedJson, FileSystem.Utf8NoBom);
+		}
+
+		return jsonResult;
+	}
+
+	protected string GetRequestData(string requestFileName){
+		FileInfo fi = new(requestFileName);
+		if (!fi.Exists) {
+			throw new FileNotFoundException("File not found", requestFileName);
+		}
+		return File.ReadAllText(requestFileName);
+	}
+
+	protected string ReplaceVariablesInJson(string json, IEnumerable<string> variables){
+		if (variables == null) {
+			return json;
+		}
+
+		foreach (string variable in variables) {
+			string pattern = "{{" + variable.Split('=')[0] + "}}";
+			Regex regex = new(pattern);
+			Match match = regex.Match(json);
+			if (match.Success) {
+				json = regex.Replace(json, variable.Split('=')[1]);
+			}
+		}
+		return json;
+	}
+	
+
+	#endregion
+
+	#region Methods: Public
+
+	public override int Execute(T options){
+		if (string.IsNullOrWhiteSpace(options.ReqeustFileName)) {
+			ExecuteServiceRequest(BuildUrl(options), string.Empty, options.ResultFileName);
+		}
+		else {
+			string requestData = GetRequestData(options.ReqeustFileName);
+			if (options.Variables != null && options.Variables.Any()) {
+				requestData = ReplaceVariablesInJson(requestData, options.Variables);
+			}
+			ExecuteServiceRequest(BuildUrl(options), requestData, options.ResultFileName);
+		}
+		return 0;
+	}
+
+	#endregion
+
+}
+
+public class DataServiceQuery : BaseServiceCommand<DataServiceQuerryOptions> {
+
+	#region Constructors: Public
+
+	public DataServiceQuery(IApplicationClient applicationClient,
+		EnvironmentSettings settings,
+		IServiceUrlBuilder serviceUrlBuilder)
+		: base(applicationClient, settings, serviceUrlBuilder){ }
+
+	#endregion
+
+	#region Methods: Protected
+
+	protected override string BuildUrl(DataServiceQuerryOptions options){
+		return options.OperationType.ToUpperInvariant() switch {
+					"SELECT" => ServiceUrlBuilderInstance.Build(ServiceUrlBuilder.KnownRoute.Select),
+					"INSERT" => ServiceUrlBuilderInstance.Build(ServiceUrlBuilder.KnownRoute.Insert),
+					"UPDATE" => ServiceUrlBuilderInstance.Build(ServiceUrlBuilder.KnownRoute.Update),
+					"DELETE" => ServiceUrlBuilderInstance.Build(ServiceUrlBuilder.KnownRoute.Delete),
+					var _ => throw new Exception("Unknown operation type")
+				};
+	}
+
+	#endregion
+
 }
