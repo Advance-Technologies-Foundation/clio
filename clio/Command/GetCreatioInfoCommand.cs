a90bc93db1b18ff40425270e13e482fd75841f95
Vladimir <nikonov.volodymyr@gmail.com>
2025-08-15 00:16:25 +0200

    Improve RemoteCommand cliogate checks and documentation
    Enhanced the RemoteCommand base class to better handle cliogate requirements by checking for both custom minimum version and service path usage. Added XML documentation comments for improved code clarity and maintainability. Minor refactoring for consistency and readability in GetCreatioInfoCommand.

--- clio/Command/GetCreatioInfoCommand.cs
+++ clio/Command/GetCreatioInfoCommand.cs
@@ -1,61 +1,62 @@
 ﻿using System.Net.Http;
-using Clio.Common;
 using CommandLine;
 using Newtonsoft.Json.Linq;
+using Clio.Common;
 
-namespace Clio.Command;
-
-#region Class: GetCreatioInfoCommandCommandOptions
-
-//clio get-info -e work
-
-[Verb("get-info", Aliases = new[] {"describe", "describe-creatio", "instance-info"},
-	HelpText = "Gets system information for Creatio instance.")]
-public class GetCreatioInfoCommandOptions : RemoteCommandOptions
-{ }
-
-#endregion
-
-#region Class: GetCreatioInfoCommandCommand
-
-public class GetCreatioInfoCommand : RemoteCommand<GetCreatioInfoCommandOptions>
+namespace Clio.Command
 {
+	#region Class: GetCreatioInfoCommandCommandOptions
 
-	#region Constructors: Public
+	//clio get-info -e work
 
-	public GetCreatioInfoCommand(IApplicationClient applicationClient, 
-		EnvironmentSettings environmentSettings, IClioGateway clioGateway)
-		: base(applicationClient, environmentSettings){
-		ClioGateWay = clioGateway;
+	[Verb("get-info", Aliases = new[] {"describe", "describe-creatio", "instance-info"},
+		HelpText = "Gets system information for Creatio instance.")]
+	public sealed class GetCreatioInfoCommandOptions : RemoteCommandOptions
+	{ }
+
+	#endregion
+
+	#region Class: GetCreatioInfoCommandCommand
+
+	public sealed class GetCreatioInfoCommand : RemoteCommand<GetCreatioInfoCommandOptions>
+	{
+
+		#region Constructors: Public
+
+		public GetCreatioInfoCommand(IApplicationClient applicationClient, 
+			EnvironmentSettings environmentSettings, IClioGateway clioGateway)
+			: base(applicationClient, environmentSettings){
+			ClioGateWay = clioGateway;
+		}
+
+		#endregion
+
+		#region Properties: Protected
+
+		protected override string ServicePath => "/rest/CreatioApiGateway/GetSysInfo";
+
+		protected override string ClioGateMinVersion { get; } = "2.0.0.32";
+
+		#endregion
+
+		#region Properties: Public
+
+		public override HttpMethod HttpMethod => HttpMethod.Get;
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void ProceedResponse(string response, GetCreatioInfoCommandOptions options){
+			base.ProceedResponse(response, options);
+			JObject jResponse = JObject.Parse(response);
+			JToken sysInfo = jResponse["SysInfo"];
+			Logger.WriteLine(sysInfo.ToString());
+		}
+
+		#endregion
+
 	}
 
 	#endregion
-
-	#region Properties: Protected
-
-	protected override string ServicePath => "/rest/CreatioApiGateway/GetSysInfo";
-
-	protected override string ClioGateMinVersion { get; } = "2.0.0.32";
-
-	#endregion
-
-	#region Properties: Public
-
-	public override HttpMethod HttpMethod => HttpMethod.Get;
-
-	#endregion
-
-	#region Methods: Protected
-
-	protected override void ProceedResponse(string response, GetCreatioInfoCommandOptions options){
-		base.ProceedResponse(response, options);
-		JObject jResponse = JObject.Parse(response);
-		JToken sysInfo = jResponse["SysInfo"];
-		Logger.WriteLine(sysInfo.ToString());
-	}
-
-	#endregion
-
 }
-
-#endregion
--- clio/Command/RemoteCommand.cs
+++ clio/Command/RemoteCommand.cs
@@ -6,139 +6,183 @@ using Clio.Common;
 using CommandLine;
 using DocumentFormat.OpenXml.Spreadsheet;
 
-namespace Clio.Command;
-
-public class RemoteCommandOptions : EnvironmentOptions
+namespace Clio.Command
 {
+    /// <summary>
+    /// Options for remote command execution.
+    /// </summary>
+    public class RemoteCommandOptions : EnvironmentOptions
+    {
+        private int? _timeOut;
+        protected virtual int DefaultTimeout { get; set; } = 100_000;
 
-	private int? _timeOut;
-	protected virtual int DefaultTimeout { get; set; } = 100_000;
-	
+        [Option("timeout", Required = false, HelpText = "Request timeout in milliseconds", Default = 100_000)]
+        public int TimeOut
+        {
+            get => _timeOut ?? DefaultTimeout;
+            internal set => _timeOut = value;
+        }
 
-	[Option("timeout", Required = false, HelpText = "Request timeout in milliseconds", Default = 100_000)]
-	public int TimeOut {
-		get => _timeOut ?? DefaultTimeout;
-		internal set => _timeOut = value;
-	}
+        public int RetryCount { get; internal set; } = 3;
+        public int RetryDelay { get; internal set; } = 1;
+    }
 
-	public int RetryCount { get; internal set; } = 3;
-	public int RetryDelay { get; internal set; } = 1;
+    /// <summary>
+    /// Base class for remote commands. Handles cliogate checks and remote execution logic.
+    /// </summary>
+    /// <typeparam name="TEnvironmentOptions">Type of environment options.</typeparam>
+    public abstract class RemoteCommand<TEnvironmentOptions> : Command<TEnvironmentOptions>
+        where TEnvironmentOptions : RemoteCommandOptions
+    {
+        #region Constructors: Protected
 
-}
+        protected RemoteCommand(IApplicationClient applicationClient,
+            EnvironmentSettings environmentSettings)
+            : this(environmentSettings)
+        {
+            ApplicationClient = applicationClient;
+        }
 
+        protected RemoteCommand(EnvironmentSettings environmentSettings)
+        {
+            EnvironmentSettings = environmentSettings;
+        }
 
-public abstract class RemoteCommand<TEnvironmentOptions> : Command<TEnvironmentOptions>
-	where TEnvironmentOptions : RemoteCommandOptions
-{
+        #endregion
 
-	#region Constructors: Protected
+        #region Constructors: Public
 
-	protected RemoteCommand(IApplicationClient applicationClient,
-		EnvironmentSettings environmentSettings)
-		: this(environmentSettings){
-		ApplicationClient = applicationClient;
-	}
+        public RemoteCommand() { } // for tests
 
-	protected RemoteCommand(EnvironmentSettings environmentSettings){
-		EnvironmentSettings = environmentSettings;
-	}
+        #endregion
 
-	#endregion
+        #region Properties: Protected
 
-	#region Constructors: Public
+        internal IApplicationClient ApplicationClient { get; set; }
+        internal EnvironmentSettings EnvironmentSettings { get; set; }
 
-	public RemoteCommand(){ } // for tests
+        /// <summary>
+        /// Minimum required cliogate version. Default is "0.0.0.0" (no requirement).
+        /// </summary>
+        protected virtual string ClioGateMinVersion { get; } = "0.0.0.0";
+        protected IClioGateway ClioGateWay { get; set; }
 
-	#endregion
+        protected string RootPath =>
+            EnvironmentSettings.IsNetCore
+                ? EnvironmentSettings.Uri : EnvironmentSettings.Uri + @"/0";
 
-	#region Properties: Protected
+        /// <summary>
+        /// Service path for remote command. If contains "rest/CreatioApiGateway", cliogate is required.
+        /// </summary>
+        protected virtual string ServicePath { get; set; }
+        protected string ServiceUri => RootPath + ServicePath;
 
-	internal IApplicationClient ApplicationClient { get; set; }
+        #endregion
 
-	internal EnvironmentSettings EnvironmentSettings { get; set; }
-	
-	
-	protected virtual string ClioGateMinVersion {get;} = "0.0.0.0";
-	protected IClioGateway ClioGateWay {get; set;}
+        #region Properties: Public
 
-	protected string RootPath =>
-		EnvironmentSettings.IsNetCore
-			? EnvironmentSettings.Uri : EnvironmentSettings.Uri + @"/0";
+        public virtual HttpMethod HttpMethod => HttpMethod.Post;
+        public int RequestTimeout { get; set; }
+        public int RetryCount { get; set; }
+        public int DelaySec { get; set; }
+        public ILogger Logger { get; set; } = ConsoleLogger.Instance;
 
-	protected virtual string ServicePath { get; set; }
+        #endregion
 
-	protected string ServiceUri => RootPath + ServicePath;
+        #region Methods: Protected
 
-	#endregion
+        /// <summary>
+        /// Executes the remote command.
+        /// </summary>
+        protected virtual void ExecuteRemoteCommand(TEnvironmentOptions options)
+        {
+            string response = HttpMethod == HttpMethod.Post
+                ? ApplicationClient.ExecutePostRequest(ServiceUri, GetRequestData(options), RequestTimeout, RetryCount, DelaySec)
+                : ApplicationClient.ExecuteGetRequest(ServiceUri, RequestTimeout, RetryCount, DelaySec);
+            ProceedResponse(response, options);
+        }
 
-	#region Properties: Public
+        /// <summary>
+        /// Gets request data for remote command.
+        /// </summary>
+        protected virtual string GetRequestData(TEnvironmentOptions options)
+        {
+            return "{}";
+        }
 
-	public virtual HttpMethod HttpMethod => HttpMethod.Post;
+        /// <summary>
+        /// Performs login to remote application.
+        /// </summary>
+        protected int Login()
+        {
+            try
+            {
+                Logger.WriteInfo($"Try login to {EnvironmentSettings.Uri} with {EnvironmentSettings.Login} credentials...");
+                ApplicationClient.Login();
+                Logger.WriteInfo("Login done");
+                return 0;
+            }
+            catch (WebException we)
+            {
+                HttpWebResponse errorResponse = we.Response as HttpWebResponse;
+                if (errorResponse.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Logger.WriteError($"Application {EnvironmentSettings.Uri} not found");
+                }
+                return 1;
+            }
+        }
 
-	public int RequestTimeout { get;  set; } 
-	public int RetryCount {  get; set; }
-	public int DelaySec {  get; set; }
+        /// <summary>
+        /// Handles response from remote command.
+        /// </summary>
+        protected virtual void ProceedResponse(string response, TEnvironmentOptions options) { }
 
-	public ILogger Logger { get; set; } = ConsoleLogger.Instance;
+        #endregion
 
-	#endregion
+        #region Methods: Public
 
-	#region Methods: Protected
+        /// <summary>
+        /// Executes the command, checks cliogate requirements and handles errors.
+        /// </summary>
+        /// <param name="options">Command options.</param>
+        /// <returns>0 if success, 1 if error.</returns>
+        public override int Execute(TEnvironmentOptions options)
+        {
+            // Check if cliogate is required
+            bool hasCustomClioGateMinVersion = !string.IsNullOrWhiteSpace(ClioGateMinVersion) && ClioGateMinVersion != "0.0.0.0";
+            bool needsCliogate = (!string.IsNullOrWhiteSpace(ServicePath) && ServicePath.Contains("rest/CreatioApiGateway"))
+                || hasCustomClioGateMinVersion;
+            if (needsCliogate && ClioGateWay == null)
+            {
+                Logger.WriteError("cliogate is not installed on the target system. This command requires cliogate.");
+                return 1;
+            }
+            if (hasCustomClioGateMinVersion && ClioGateWay != null)
+            {
+                ClioGateWay.CheckCompatibleVersion(ClioGateMinVersion);
+            }
+            try
+            {
+                RequestTimeout = options.TimeOut;
+                RetryCount = options.RetryCount;
+                DelaySec = options.RetryDelay;
+                ExecuteRemoteCommand(options);
+                string commandName = typeof(TEnvironmentOptions).GetCustomAttribute<VerbAttribute>()?.Name;
+                Logger.WriteInfo($"Done {commandName}");
+                return 0;
+            }
+            catch (SilentException)
+            {
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Logger.WriteError(e.Message);
+                return 1;
+            }
+        }
 
-	protected virtual void ExecuteRemoteCommand(TEnvironmentOptions options){
-		string response = HttpMethod == HttpMethod.Post
-			? ApplicationClient.ExecutePostRequest(ServiceUri, GetRequestData(options), RequestTimeout, RetryCount, DelaySec)
-			: ApplicationClient.ExecuteGetRequest(ServiceUri, RequestTimeout, RetryCount, DelaySec);
-		ProceedResponse(response, options);
-	}
-
-	protected virtual string GetRequestData(TEnvironmentOptions options){
-		return "{}";
-	}
-
-	protected int Login(){
-		try {
-			Logger.WriteInfo($"Try login to {EnvironmentSettings.Uri} with {EnvironmentSettings.Login} credentials...");
-			ApplicationClient.Login();
-			Logger.WriteInfo("Login done");
-			return 0;
-		} catch (WebException we) {
-			HttpWebResponse errorResponse = we.Response as HttpWebResponse;
-			if (errorResponse.StatusCode == HttpStatusCode.NotFound) {
-				Logger.WriteError($"Application {EnvironmentSettings.Uri} not found");
-			}
-			return 1;
-		}
-	}
-
-	protected virtual void ProceedResponse(string response, TEnvironmentOptions options){ }
-
-	#endregion
-
-	#region Methods: Public
-
-	public override int Execute(TEnvironmentOptions options){
-		
-		if(!string.IsNullOrWhiteSpace(ClioGateMinVersion) && ClioGateWay != null) {
-			ClioGateWay.CheckCompatibleVersion(ClioGateMinVersion);
-		}
-		
-		try {
-			RequestTimeout = options.TimeOut;
-			RetryCount = options.RetryCount;
-			DelaySec = options.RetryDelay;
-			ExecuteRemoteCommand(options);
-			string commandName = typeof(TEnvironmentOptions).GetCustomAttribute<VerbAttribute>()?.Name;
-			Logger.WriteInfo($"Done {commandName}");
-			return 0;
-		} catch (SilentException) {
-			return 1;
-		} catch (Exception e) {
-			Logger.WriteError(e.Message);
-			return 1;
-		}
-	}
-
-	#endregion
-
+        #endregion
+    }
 }
