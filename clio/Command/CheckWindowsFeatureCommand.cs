446e91928ebe05aa0d9cfe518d12b414d3c36bba
k.krylov <k.krylov@creatio.com>
2025-11-05 10:34:34 +0100

    refactor: improve CheckWindowsFeaturesCommand structure and readability
    - Reorganized using directives and added regions for better code structure
    - Updated class and method definitions for consistency
    - Enhanced logging messages for clarity
    - Fixed minor typos in variable names and comments

--- clio/Command/CheckWindowsFeatureCommand.cs
+++ clio/Command/CheckWindowsFeatureCommand.cs
@@ -1,51 +1,60 @@
-﻿using Clio.Common;
-using CommandLine;
+﻿#region
+
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Management;
+using Clio.Common;
+using CommandLine;
 
-namespace Clio.Command
-{
-	[Verb("check-windows-features", Aliases = new string[] { "checkw", "checks", "checkf", "cf", "check-windows-features" }, HelpText = "Check current windows for requirment components")]
-	public class CheckWindowsFeaturesOptions
-	{
+#endregion
 
+namespace Clio.Command;
+
+[Verb("check-windows-features", Aliases = ["checkw", "checks", "checkf", "cf", "check-windows-features"]
+	, HelpText = "Check windows for the required components")]
+public class CheckWindowsFeaturesOptions{ }
+
+public class CheckWindowsFeaturesCommand : Command<CheckWindowsFeaturesOptions>{
+	#region Fields: Private
+
+	private readonly ILogger _logger;
+	private readonly IWindowsFeatureManager _windowsFeatureManager;
+
+	#endregion
+
+	#region Constructors: Public
+
+	public CheckWindowsFeaturesCommand(IWindowsFeatureManager windowsFeatureManager, ILogger logger) {
+		_windowsFeatureManager = windowsFeatureManager;
+		_logger = logger;
 	}
 
-	public class CheckWindowsFeaturesCommand : Command<CheckWindowsFeaturesOptions>
-	{
-		private IWindowsFeatureManager _windowsFeatureManager;
-		private readonly ILogger _logger;
+	#endregion
 
-		public CheckWindowsFeaturesCommand(IWindowsFeatureManager windowsFeatureManager, ILogger logger){
-			_windowsFeatureManager = windowsFeatureManager;
-			_logger = logger;
+	#region Methods: Public
+
+	public override int Execute(CheckWindowsFeaturesOptions options) {
+		List<WindowsFeature> missedComponents = _windowsFeatureManager.GetMissedComponents();
+		IEnumerable<WindowsFeature> requiredComponentStates = _windowsFeatureManager.GerRequiredComponent();
+		_logger.WriteLine(
+			"For detailed information visit: https://academy.creatio.com/docs/user/on_site_deployment/application_server_on_windows/check_required_components/enable_required_windows_components");
+		_logger.WriteInfo($"{Environment.NewLine}Check started:");
+		foreach (WindowsFeature item in requiredComponentStates) {
+			_logger.WriteInfo($"{item}");
 		}
 
-		public override int Execute(CheckWindowsFeaturesOptions options) {
-			var missedComponents = _windowsFeatureManager.GetMissedComponents();
-			var requirmentComponentStates = _windowsFeatureManager.GerRequiredComponent();
-			_logger.WriteLine("For detailed information visit: https://academy.creatio.com/docs/user/on_site_deployment/application_server_on_windows/check_required_components/enable_required_windows_components");
-			_logger.WriteInfo($"{Environment.NewLine}Check started:");
-			foreach (var item in requirmentComponentStates) {
+		_logger.WriteLine("");
+		if (missedComponents.Count > 0) {
+			_logger.WriteError("Windows has missed components:");
+			foreach (WindowsFeature item in missedComponents) {
 				_logger.WriteInfo($"{item}");
 			}
-			_logger.WriteLine("");
-			if (missedComponents.Count > 0) {
-				_logger.WriteInfo("Windows has missed components:");
-				foreach (var item in missedComponents) {
-					_logger.WriteInfo($"{item}");
-				}
-				return 1;
-			} else {
-				_logger.WriteError("All requirment components installed");
-				return 0;
-			}
+
+			return 1;
 		}
 
-
-
+		_logger.WriteInfo("All requirement components installed");
+		return 0;
 	}
+
+	#endregion
 }
