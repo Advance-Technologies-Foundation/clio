41304edbb02103fb9f9576264a2b94823ae464fd
Vladimir <nikonov.volodymyr@gmail.com>
2025-11-27 23:35:12 +0100

    Add tests for Hosts and Stop commands, update WikiAnchors
    Introduced HostsCommandTests and StopCommandTests to verify documentation for the respective commands. Updated WikiAnchors.txt to include anchors for 'hosts' and 'stop' commands.

---
+++ clio.tests/Command/HostsCommandTests.cs
@@ -0,0 +1,10 @@
+using Clio.Command;
+using NUnit.Framework;
+
+namespace Clio.Tests.Command;
+
+[TestFixture]
+[Description("Tests for HostsCommand - verifies documentation exists for hosts command")]
+public class HostsCommandTests : BaseCommandTests<HostsOptions>
+{
+}
---
+++ clio.tests/Command/StopCommandTests.cs
@@ -0,0 +1,10 @@
+using Clio.Command;
+using NUnit.Framework;
+
+namespace Clio.Tests.Command;
+
+[TestFixture]
+[Description("Tests for StopCommand - verifies documentation exists for stop command")]
+public class StopCommandTests : BaseCommandTests<StopOptions>
+{
+}
