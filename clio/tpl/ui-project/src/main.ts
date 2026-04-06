a50d0f56123ddb3562732a997ee063691632b989
a.trofimchuk <a.trofimchuk@creatio.com>
2026-04-06 18:30:47 +0300

    ENG-81342 ENG-88036 Lazy remotes: Update templates

--- clio/tpl/ui-project/src/main.ts
+++ clio/tpl/ui-project/src/main.ts
@@ -1,8 +1,9 @@
 ﻿import type { RemoteEntryDefinition } from '@creatio-devkit/common';
 import { designFeatureDefinition } from './app/features/design/design.feature-definition';
 import { runtimeFeatureDefinition } from './app/features/runtime/runtime.feature-definition';
+import { REMOTE_ENTRY_NAME } from './app/remote-entry.ids';
 
 export default {
-  remoteName: '<%projectName%>',
+  remoteName: REMOTE_ENTRY_NAME,
   features: [runtimeFeatureDefinition, designFeatureDefinition],
 } satisfies RemoteEntryDefinition;
