bb45652db726e461c70f1cad88cc6e576c00f052
Dmytro Tokarev <d.tokarev@creatio.com>
2026-07-03 12:12:46 +0300

    #ENG-92791 The title should be set for the replacement schema in case of updating dashboard
    AI agents: none

--- clio/Command/PageUpdateOptions.cs
+++ clio/Command/PageUpdateOptions.cs
@@ -570,7 +570,7 @@ namespace Clio.Command {
 			dto["name"] = originalName;
 			dto["isReadOnly"] = false;
 			dto["extendParent"] = true;
-			dto["caption"] = null;
+			dto["caption"] = template["caption"]?.DeepClone();
 			dto[LocalizableStringsKey] = template[LocalizableStringsKey]?.DeepClone() ?? new JArray();
 			dto["package"] = new JObject {
 				["uId"] = context.DesignPackageUId,
